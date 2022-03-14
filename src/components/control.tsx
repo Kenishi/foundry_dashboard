a9ee845582e72c147d44c01c9c7838da0971228e
Jeremy May <kenishi86@gmail.com>
2022-03-14 18:34:23 -0400

    Add code for control

--- src/components/control.tsx
+++ src/components/control.tsx
@@ -1,9 +1,33 @@
+import { Button, Grid } from "@mui/material";
+import Box from "@mui/material/Box";
+import { useState } from "react";
+import { useQuery } from "react-query";
+
 export const Control = () => {
-    
+    const [ status, setStatus ] = useState("unknown");
+
+    async function fetchStatus() { 
+        const result = await fetch("http://foundry-dashboard-api:45321/status");
+        console.log(result);
+    }
 
     return (
         <div>
-            My control
+            <Grid container>
+                <Grid xs={12}>Foundry Server</Grid>
+                <Grid xs={6}>Status</Grid>
+                <Grid xs={6}>${status}</Grid>
+                <Grid xs={12}>
+                    <Box>
+                        <Grid xs={6}>
+                            <Button>Restart</Button>
+                        </Grid>
+                        <Grid xs={6}>
+                            <Button>Update Server</Button>
+                        </Grid>
+                    </Box>
+                </Grid>
+            </Grid>
         </div>
     )
 };
